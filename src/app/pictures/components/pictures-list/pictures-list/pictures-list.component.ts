2b3e56cf3f08bafbb3bb05c5758e67169574187c
André <andre.escocard@nttdata.com>
2022-03-22 00:12:38 -0300

    model implementation

--- src/app/pictures/components/pictures-list/pictures-list/pictures-list.component.ts
+++ src/app/pictures/components/pictures-list/pictures-list/pictures-list.component.ts
@@ -1,9 +1,25 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component } from '@angular/core';
+import { Picture } from 'src/app/pictures/pictures.models';
 
 @Component({
   selector: 'pictures-list',
   templateUrl: './pictures-list.component.html'
 })
 export class PicturesListComponent {
-  images=['img1','img2','img3']
+  images: Picture[];
+  
+  constructor(){
+    this.images = [
+      {
+        img_src: 'https://mars.nasa.gov/system/resources/detail_files/26526_PIA25046-web.jpg'
+      },
+      {
+        img_src: 'https://www.nasa.gov/sites/default/files/thumbnails/image/pia21044_orig.jpg'
+      },
+      {
+        img_src: 'https://www.nasa.gov/sites/default/files/thumbnails/image/pia22313.jpg'
+      }
+    ];
+  }
 }
