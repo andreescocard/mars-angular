dc121b65f7a543e13e8e4d774250d1d7c1e843d9
André <andre.escocard@nttdata.com>
2022-04-04 12:10:57 -0300

    feat: missing one file from module 1

--- src/app/pictures/pictures.module.ts
+++ src/app/pictures/pictures.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PicturesListComponent } from "./components/pictures-list/pictures-list/pictures-list.component";
 import { PicturesService } from "./pictures.service";
+import { PictureItemComponent } from './components/picture-item/picture-item.component';
 
 @NgModule({
     imports:[
         CommonModule
     ],
   declarations: [
-    PicturesListComponent
+    PicturesListComponent,
+    PictureItemComponent
   ], 
   exports: [PicturesListComponent],
   providers: [ 
